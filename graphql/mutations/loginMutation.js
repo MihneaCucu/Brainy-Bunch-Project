fc71cec385510b3c2e1b0a593f8e2f33ef519932
MihneaCucu <cucumihnea0@gmail.com>
2025-12-13 13:23:17 +0200

    fix: login and genreQuery

--- graphql/mutations/loginMutation.js
+++ graphql/mutations/loginMutation.js
@@ -27,27 +27,37 @@ const loginMutation = {
     resolve: async (_, args) => {
         const { username, password } = args.input;
 
+        console.log('Login attempt:', { username, password: password ? password : 'undefined' });
+
         const user = await db.User.findOne({ where: { username } });
 
         if(!user) {
+            console.log('User not found:', username);
             return {
-              reason: "Incorrect Password",
+              reason: "User not found",
             }
         }
 
+        console.log('User found:', { id: user.id, username: user.username, hasPassword: !!user.password });
+
         const passwordMatch = await bcrypt.compare(password, user.password);
+
+
+        console.log('Password match:', passwordMatch);
 
         if (passwordMatch) {
             const token = jwt.sign({
                 sub: user.id,
             }, JWT_SECRET_KEY);
 
+            console.log('Login successful, token generated');
             return {
                 id: user.id,
                 token,
             };
         }
 
+        console.log('Password mismatch');
         return {
           reason: "Incorrect Password",
         }
