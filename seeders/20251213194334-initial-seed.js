7918d52e634be800bbb227b14bc69b06a2020668
AntonioGrigorascu <grigorascu.antonio@gmail.com>
2025-12-13 21:54:49 +0200

    Added Users and Roles seeder

---
+++ seeders/20251213194334-initial-seed.js
@@ -0,0 +1,53 @@
+'use strict';
+const bcrypt = require('bcrypt');
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    
+    // Create Roles
+    await queryInterface.bulkInsert('Roles', [
+      { id: 1, name: 'user', createdAt: new Date(), updatedAt: new Date() },
+      { id: 2, name: 'moderator', createdAt: new Date(), updatedAt: new Date() },
+      { id: 3, name: 'admin', createdAt: new Date(), updatedAt: new Date() }
+    ], {});
+
+    // One hashed password to use for all test users
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash('UserPa55!', salt);
+
+    // Create Users
+    await queryInterface.bulkInsert('Users', [
+      {
+        username: 'RegularUser',
+        email: 'user@example.com',
+        password: hashedPassword,
+        roleId: 1, // 'user' role
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        username: 'ModeratorUser',
+        email: 'mod@example.com',
+        password: hashedPassword,
+        roleId: 2, // 'moderator' role
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        username: 'AdminUser',
+        email: 'admin@example.com',
+        password: hashedPassword,
+        roleId: 3, // 'admin' role
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    // delete data in reverse order (Users first, then Roles) 
+    // to avoid foreign key constraint errors
+    await queryInterface.bulkDelete('Users', null, {});
+    await queryInterface.bulkDelete('Roles', null, {});
+  }
+};
