5491a81ccee0fdfa60c8c999c46ded8f3adfc6ae
AlexCretu20 <alexmari.cretu@gmail.com>
2026-01-13 22:01:19 +0200

    My dairy happy and sad paths

--- tests/resolvers/myDiary.test.js
+++ tests/resolvers/myDiary.test.js
@@ -4,34 +4,43 @@ const MyDiary = require('../../graphql/queries/diary/myDiary');
 setupTestDB();
 
 describe('Query: myDiary', () => {
-    it('should require authentication', async () => {
-        await expect(MyDiary.resolve(null, {}, {})).rejects.toThrow('Unauthentificated: Please log in');
+    describe('Happy path', () => {
+        it('should require authentication', async () => {
+            await expect(MyDiary.resolve(null, {}, {})).rejects.toThrow('Unauthentificated: Please log in');
+        });
+
+        it('should create a diary if none exists and return it', async () => {
+            const userRole = await db.Role.create({ name: 'user' });
+            const user = await db.User.create({ username: 'owner1', email: 'owner1@test.com', password: 'Owner1Pa55!', roleId: userRole.id });
+
+            const context = { user: { id: user.id, userRole: { name: 'user' } } };
+
+            const res = await MyDiary.resolve(null, {}, context);
+            expect(res).toBeDefined();
+            expect(res.userId).toBe(user.id);
+
+            const found = await db.Diary.findOne({ where: { userId: user.id } });
+            expect(found).toBeDefined();
+            expect(found.id).toBe(res.id);
+        });
+
+        it('should return existing diary for the user', async () => {
+            const userRole = await db.Role.create({ name: 'user' });
+            const user = await db.User.create({ username: 'owner2', email: 'owner2@test.com', password: 'Owner2Pa55!', roleId: userRole.id });
+
+            const diary = await db.Diary.create({ userId: user.id });
+
+            const context = { user: { id: user.id, userRole: { name: 'user' } } };
+            const res = await MyDiary.resolve(null, {}, context);
+            expect(res).toBeDefined();
+            expect(res.id).toBe(diary.id);
+        });
     });
 
-    it('should create a diary if none exists and return it', async () => {
-        const userRole = await db.Role.create({ name: 'user' });
-        const user = await db.User.create({ username: 'owner1', email: 'owner1@test.com', password: 'Owner1Pa55!', roleId: userRole.id });
+    describe('Sad path', () => {
+        it('should throw error if user is not authenticated', async () => {
+            await expect(MyDiary.resolve(null,{}, {})).rejects.toThrow('Unauthentificated: Please log in');
+        })
+    })
 
-        const context = { user: { id: user.id, userRole: { name: 'user' } } };
-
-        const res = await MyDiary.resolve(null, {}, context);
-        expect(res).toBeDefined();
-        expect(res.userId).toBe(user.id);
-
-        const found = await db.Diary.findOne({ where: { userId: user.id } });
-        expect(found).toBeDefined();
-        expect(found.id).toBe(res.id);
-    });
-
-    it('should return existing diary for the user', async () => {
-        const userRole = await db.Role.create({ name: 'user' });
-        const user = await db.User.create({ username: 'owner2', email: 'owner2@test.com', password: 'Owner2Pa55!', roleId: userRole.id });
-
-        const diary = await db.Diary.create({ userId: user.id });
-
-        const context = { user: { id: user.id, userRole: { name: 'user' } } };
-        const res = await MyDiary.resolve(null, {}, context);
-        expect(res).toBeDefined();
-        expect(res.id).toBe(diary.id);
-    });
 });
